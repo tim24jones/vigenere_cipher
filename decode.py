bc29eaa2910109f0e7070aa65d801df2fcb144c3
Tim Jones <tim.24.jones@gmail.com>
2017-10-07 23:14:08 -0400

    Create decode.py

---
+++ decode.py
@@ -0,0 +1,52 @@
+notes for decoding process:
+
+#letter frequency stats:
+#space:16.393
+#E:10.44
+#T:7.759
+#A:6.722
+#O:6.388
+#I:6.329
+#N:6.045
+#S:5.443
+#R:5.251
+#H:4.222
+#L:3.403
+#D:3.194
+#C:2.792
+#U:2.282
+#M:2.099
+#F:2.007
+#P:1.789
+#G:1.563
+#W:1.405
+#Y:1.388
+#B:1.237
+#,
+#.
+#V:0.8779
+#'
+#"
+#-
+#K:0.4515
+#X:0.1923
+#0
+#J:0.1338
+#1
+#Q:0.1003
+#2
+#Z:0.0752
+#then ():!?5;349/867[]%$|*=_+>\<&^#@`~{}
+
+#can use enchant for spell checking:
+#import enchant
+#d=enchant.Dict("en_US") 
+#or maybe British english could be an option
+#d.check("Hello")
+#d.check("Helo")
+#d.suggest("Helo")
+
+#decode with known key length (probably recommended as first step), detect possible words
+#decode probably limited to letters only initially
+#could display version with most English words or letters within words
+#extend by repeating for increased key length
